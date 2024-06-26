3a28e59af98b5e1a98176c72011be1e04ef94222
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-06-26 16:30:31 -0400

    Add queryselectors

--- script/index.js
+++ script/index.js
@@ -1,3 +1,25 @@
+let display = document.querySelector('#display');
+let clearButton = document.querySelector('#clearButton');
+let positiveNegativeButton = document.querySelector('#positiveNegativeButton');
+let percentageButton = document.querySelector('#percentageButton');
+let sevenButton = document.querySelector('#sevenButton');
+let eightButton = document.querySelector('#eightButton');
+let nineButton = document.querySelector('#nineButton');
+let fourButton = document.querySelector('#fourButton');
+let fiveButton = document.querySelector('#fiveButton');
+let sixButton = document.querySelector('#sixButton');
+let oneButton = document.querySelector('#oneButton');
+let twoButton = document.querySelector('#twoButton');
+let threeButton = document.querySelector('#threeButton');
+let zeroButton = document.querySelector('#zeroButton');
+let decimalButton = document.querySelector('#decimalButton');
+let divideButton = document.querySelector('#divideButton');
+let multiplyButton = document.querySelector('#multiplyButton');
+let subtractButton = document.querySelector('#subtractButton');
+let addButton = document.querySelector('#addButton');
+let equalButton = document.querySelector('#equalButton');
+
+
 let numberOne;
 let numberTwo;
 let operator;
