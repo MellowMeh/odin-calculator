687da658605e759b7cb96b7edb18fc25723c85eb
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-06-26 16:07:13 -0400

    Add arithmetic functions

---
+++ script/index.js
@@ -0,0 +1,26 @@
+let numberOne;
+let numberTwo;
+let operator;
+
+let divideNumbers = () => {
+    result = numberOne / numberTwo;
+    return result;
+}
+
+let multiplyNumbers = () => {
+    result = numberOne * numberTwo;
+    return result;
+}
+
+let addNumbers = () => {
+    result = numberOne + numberTwo;
+    return result;
+}
+
+let subtractNumbers = () => {
+    result = numberOne - numberTwo;
+    return result;
+}
+
+
+
